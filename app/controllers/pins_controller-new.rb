197f3b5159246e1b356c9680abb23771e9ab03fe
Camie DeCovich <camiedecovich@gmail.com>
2015-08-21 12:16:46 -0800

    Added new pin form.

--- app/controllers/pins_controller-new.rb
+++ app/controllers/pins_controller-new.rb
@@ -5,21 +5,21 @@ class PinsController < ApplicationController
   end
 
   # GET /pins/new
-  def new
-    @pin = Pin.new
-    #new_pin_path
-  end
-  
   # Create a new pin
   def create
+    #@pin = Pin.new(pin_params)
     @pin = Pin.new(params)
   end
-
 
   def show
     @pin = Pin.find(params[:id])
   end
   
+  private
+  def pin_params
+    params.require(:pin).permit(:title, :url, :slug, :text, :resource_type)
+  end
+
   # Search for a Pin using the slug in the URL
   def show_by_name
     @pin = Pin.find_by_slug(params[:slug])
