c34e63b114a23547fcc4ea43e4862188cab441c8
Camie DeCovich <camiedecovich@gmail.com>
2015-08-21 18:54:00 -0800

    Added create action with strong parameters. Added form validation.

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -4,7 +4,19 @@ class PinsController < ApplicationController
     @pins = Pin.all
   end
 
-  # GET /pins/new
+  def show
+    #@pin = Pin.find(pin_params)
+    @pin = Pin.find(params[:id])
+  end
+
+
+  # Search for a Pin using the slug in the URL
+  def show_by_name
+    @pin = Pin.find_by_slug(params[:slug])
+    render :show
+  end
+
+# GET /pins/new
   def new
     @pin = Pin.new
     #new_pin_path
@@ -12,18 +24,21 @@ class PinsController < ApplicationController
   
   # Create a new pin
   def create
-    @pin = Pin.new(params)
-  end
-
-
-  def show
-    @pin = Pin.find(params[:id])
+    @pin = Pin.create(pin_params)
+    #@pin = Pin.create(params[:pin])
+    if @pin.valid?
+      @pin.save
+      redirect_to pin_path(@pin)
+    else
+      @errors = @pin.errors
+      render :new
+    end
   end
   
-  # Search for a Pin using the slug in the URL
-  def show_by_name
-    @pin = Pin.find_by_slug(params[:slug])
-    render :show
-  end
+    private
+    def pin_params
+      params.require(:pin).permit(:title, :url, :slug, :text, :category_id)
+    end
 
+  
 end
